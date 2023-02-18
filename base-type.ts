1a054097cfdbde5191981015b08cb8378704fa75
lovely-man <liangyanxiangde@163.com>
2023-02-18 14:43:03 +0800

    基本类型

---
+++ base-type.ts
@@ -0,0 +1,16 @@
+let num: number = 0;
+num = 23
+function fn(a: string) {
+    a += 'ffff';
+}
+
+fn('1323')
+
+let flag: boolean = true;
+// flag = 2324
+flag = false;
+
+let num1: number = 10;
+num1 = 0xfff;
+num1 = 0b101010
+num1 = 0o234324
---
+++ dist/base-type.js
@@ -0,0 +1,14 @@
+"use strict";
+var num = 0;
+num = 23;
+function fn(a) {
+    a += 'ffff';
+}
+fn('1323');
+var flag = true;
+// flag = 2324
+flag = false;
+var num1 = 10;
+num1 = 0xfff;
+num1 = 42;
+num1 = 80084;
